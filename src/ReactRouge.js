56b57093a4ef29e208b5482e16e8aaaf36c72169
Urva Suthar <urvasuthar2@gmail.com>
2023-06-04 11:33:40 +0530

    inputmanger added

--- src/ReactRouge.js
+++ src/ReactRouge.js
@@ -1,21 +1,23 @@
-import React,{useRef , useEffect} from 'react'
+import React, { useRef, useEffect } from 'react';
 
 const ReactRouge = ({ width, height, tilesize }) => {
-	const canvasRef = useRef();
-	useEffect(()=>{
-		const canvas = canvasRef.current;
-		const ctx = canvas.getContext('2d');
-		ctx.fillStyle = '#000';
-		ctx.fillRect(0, 0, width * tilesize, height * tilesize);
-	});
+  const canvasRef = useRef();
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+    ctx.fillStyle = '#000';
+    ctx.fillRect(12, 12, 16, 16);
+  }, [width, height, tilesize]);
 
-	return (
-		<canvas
-			width={width * tilesize}
-			height={height * tilesize}
-			style={{ border: '1px solid black' }}
-		></canvas>
-	);
-}
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width * tilesize}
+      height={height * tilesize}
+      style={{ border: '1px solid black' }}
+    ></canvas>
+  );
+};
 
-export default ReactRouge
+export default ReactRouge;
